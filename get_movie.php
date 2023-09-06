f8daa1a7be5a587ac2ce29495bc90bece4b19c4c
A-Bergfeldt <bergfeldt.andreas@gmail.com>
2023-09-06 17:01:58 +0200

    fixed search

--- get_movie.php
+++ get_movie.php
@@ -1,27 +1,32 @@
 <?php
-$servername = "localhost:80";
+error_reporting(E_ERROR | E_PARSE);
+
+$servername = "127.0.0.1";
 $username = "root";
-$password = "root";
+$password = "";
 $dbname = "things";
 
 // Create connection
 $link = mysqli_connect($servername, $username, $password, $dbname); 
 
+$search = $_GET['search'];
+
 if (mysqli_connect_error()) { 
     die("Connection failed: " . mysqli_connect_error());  
 }
-echo '<form> Search for a movie here:  <input type="text" name="search_text"><input type="submit" value="Search">';
+echo '<form action="get_movie.php" method="GET"> Search for a movie here:  <input type="text" name="search"><input type="submit" value="Search"><br>';
 
-$sql2 = "SELECT mname FROM movies WHERE mname LIKE '%$search%'";
+$sql2 = "SELECT * FROM `movies` INNER JOIN genres ON genres.gid=movies.mgenreid WHERE mname LIKE '%$search%'";
 
-$result = $conn->query($sql2);
-
-if ($result->num_rows > 0){
-while($row = $result->fetch_assoc() ){
-	echo "<tr><td>" . $row["mname"] . "</td><td>" . $row["myear"] . "</td><td>" . $row["mgenre"] . "</td><td>" . $row["mrating"] . "</td></tr>";
-}
-} else {
-	echo "0 records";
+$result = $link->query($sql2);
+if (! empty( $search)) {
+    if ($result->num_rows > 0){
+    while($row = $result->fetch_assoc() ){
+        echo "<tr><td>" . $row["mname"] . "</td><td>" . $row["myear"] . "</td><td>" . $row["mgenre"] . "</td><td>" . $row["mrating"] . "</td></tr><br>";
+    }
+    } else {
+        echo "0 records";
+    }
 }
 
 echo '<table><tr><th>Name of movie</th><th>Year of release</th><th>Genre of movie</th><th>Rating</th></tr>';
